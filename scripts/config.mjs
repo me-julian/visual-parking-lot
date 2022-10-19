818f118612ff73cd6c703e32978b507ab9f74f8d
Julian Edwards <julian.michael.edwards@gmail.com>
2022-10-19 13:17:55 -0500

    Increased parking duration range.

--- scripts/config.mjs
+++ scripts/config.mjs
@@ -1,16 +1,21 @@
 'use strict'
 
 let config = {
+    // ms
     LOOP_SPEED: 50,
+    // secs
     carSpawnRate: {
         min: 5,
         max: 20,
     },
+    // 1 / handicapChance
     handicapChance: 15,
+    // ms
     spawnCarCooldownTime: 2000,
+    // secs
     carParkingDurationRange: {
-        min: 30,
-        max: 180,
+        min: 60,
+        max: 540,
     },
 }
 
